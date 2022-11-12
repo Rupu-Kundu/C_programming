985e175ff967a8f453072727a2ae921bce4e5751
rupu <promamondolrupu@gmail.com>
2022-11-13 01:44:40 +0600

    uplodding solution sol_5

---
+++ Tasks/Task-01/solutions/sol_5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number :\n");
+    scanf("%d",&num);
+    if(num%2 ==0)
+
+        printf("even is %d",num);
+
+    else
+
+    printf("odd is %d",num);
+    return 0;
+
+}
+
+
